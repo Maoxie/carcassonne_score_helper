b3081136b7674cea4a87894acfa7cc03750245b4
Maoxie <edwardyzt@gmail.com>
2018-01-17 23:21:24 +0800

    bug fixed

--- js/carcassonne.js
+++ js/carcassonne.js
@@ -1,61 +1,3 @@
-// let player_num_select = $("#player_num_select")
-// let num_of_players = eval(player_num_select.val())
-
-// let score_bars = $("#score_bars")
-// let score_bar_html = $("#score_bars").html()
-
-// player_num_select.on("change", function () {
-//     num_of_players = eval(player_num_select.val())
-// })
-
-// $(document).ready(function () {
-//     let score_to_add = 0
-//     let scores = []
-//     for (let i = num_of_players - 1; i >= 0; i--) {
-//         scores[i] = 0
-//     }
-    
-//     score_bars_init(num_of_players)
-
-//     // eval( .substring(1))
-// })
-
-
-// let score_bars_init = function(n_players){
-//     let score_bars_html = ""
-//     for (let i = 0; i < n_players; i++) {
-//         score_bars_html += score_bar_html.format({player: "i", percent: "0", score: "&ensp;&ensp;0"})
-//     }
-
-//     score_bars.html(score_bars_html)
-// }
-
-
-// // 格式化字符串
-// String.prototype.format = function(args) {
-// 	var result = this;
-// 	if (arguments.length > 0) {    
-// 		if (arguments.length == 1 && typeof (args) == "object") {
-// 			for (var key in args) {
-// 				if(args[key]!=undefined){
-// 					var reg = new RegExp("({" + key + "})", "g");
-// 					result = result.replace(reg, args[key]);
-// 				}
-// 			}
-// 		}
-// 		else {
-// 			for (var i = 0; i < arguments.length; i++) {
-// 				if (arguments[i] != undefined) {
-// 					var reg = new RegExp("({[" + i + "]})", "g");
-// 					result = result.replace(reg, arguments[i]);
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return result;
-// }
-
-
 app = new Vue({
 	el: '#app',
 	data: {
@@ -96,17 +38,17 @@ app = new Vue({
 				return
 			}
 			for (let index in this.checkedPlayers) {
-				this.players[this.checkedPlayers[index]].value += this.toAdd
+				this.players[this.checkedPlayers[index]].score += this.toAdd
 			}
 			this.toAdd = 0
 			let max_score = 0
 			for (let i = this.selected - 1; i >= 0; i--) {
-				if (max_score < this.players[i].value) {
-					max_score = this.players[i].value
+				if (max_score < this.players[i].score) {
+					max_score = this.players[i].score
 				}
 			}
 			for (let i = this.selected - 1; i >= 0; i--) {
-				this.players[i].percentage = this.players[i].value / max_score * 100
+				this.players[i].percentage = this.players[i].score / max_score * 100
 			}
 			this.checkedPlayers = []
 		}
